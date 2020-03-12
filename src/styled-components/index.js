39d9edf539f11c0d64ae2a309744fd9973274992
Alexis Troncoso <alexis.troncoso@opinno.com>
2020-03-12 08:54:47 +0100

    chore: install dependencies
    add styled components and ramda

---
+++ src/styled-components/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const CustomButton = styled.div`
+  display: block;
+  background-color: #eeffdd;
+  padding: 10px;
+  cursor: pointer;
+`
+
+export default CustomButton
